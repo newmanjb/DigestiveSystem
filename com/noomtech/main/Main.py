3ec9448f93e2ca58b8c4ff490c2ef631dcb96e8c
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-10 08:56:07 +0100

    Update Main.py
    Added some tests for the mouth's chewing and swallowing

--- com/noomtech/main/Main.py
+++ com/noomtech/main/Main.py
@@ -1,15 +1,43 @@
 from com.noomtech.consumables.foods import Meat, Vegetable
+from com.noomtech.consuming.Mouth import Mouth
+from time import sleep
 
 """Test to make sure that different foods can be chewed in different amounts of time"""
 
 if __name__ == "__main__":
 
-    meat = Meat()
-    vegetable = Vegetable()
+    meat1 = Meat()
+    vegetable1 = Vegetable()
+    meat2 = Meat()
+    vegetable2 = Vegetable()
+    meat3 = Meat()
+    vegetable3 = Vegetable()
+    meat4 = Meat()
+    vegetable4 = Vegetable()
+    meat5 = Meat()
+    vegetable5 = Vegetable()
+    meat6 = Meat()
+    vegetable6 = Vegetable()
 
-    print(meat.state)
-    print(vegetable.state)
-    meat.chew()
-    vegetable.chew()
-    print(meat.state)
-    print(vegetable.state)
+    mouth = Mouth();
+    mouth.start()
+
+    mouth.add(meat1)
+    mouth.add(vegetable1)
+
+    sleep(1)
+
+    mouth.add(meat2)
+    mouth.add(vegetable2)
+
+    sleep(1)
+
+    mouth.add(meat3)
+    mouth.add(vegetable3)
+
+    sleep(1)
+
+    mouth.add(meat4)
+    mouth.add(vegetable4)
+
+    sleep(1)

bdc7b27cf48e67c2ced37a90d30c93a134387420
Joshua Newman <joshuajmnewman@gmail.com>
2018-07-10 11:06:03 +0100

    Update Main.py
    Added a test for the new Pipe class

--- com/noomtech/main/Main.py
+++ com/noomtech/main/Main.py
@@ -1,43 +1,56 @@
-from com.noomtech.consumables.foods import Meat, Vegetable
-from com.noomtech.consuming.Mouth import Mouth
 from time import sleep
+
+from com.noomtech.consuming import Mouth
+from com.noomtech.building_blocks import Pipe
+from com.noomtech.consumables.foods import Meat, Vegetable
 
 """Test to make sure that different foods can be chewed in different amounts of time"""
 
 if __name__ == "__main__":
 
-    meat1 = Meat()
-    vegetable1 = Vegetable()
-    meat2 = Meat()
-    vegetable2 = Vegetable()
-    meat3 = Meat()
-    vegetable3 = Vegetable()
-    meat4 = Meat()
-    vegetable4 = Vegetable()
-    meat5 = Meat()
-    vegetable5 = Vegetable()
-    meat6 = Meat()
-    vegetable6 = Vegetable()
+    pipe1 = Pipe(4);
+    pipe1.start()
 
-    mouth = Mouth();
-    mouth.start()
+    pipe1.receive([Meat(),Vegetable])
+    sleep(8)
+    pipe1.receive([Meat(),Vegetable])
+    sleep(8)
+    pipe1.stop()
 
-    mouth.add(meat1)
-    mouth.add(vegetable1)
+    # meat1 = Meat()
+    # vegetable1 = Vegetable()
+    # meat2 = Meat()
+    # vegetable2 = Vegetable()
+    # meat3 = Meat()
+    # vegetable3 = Vegetable()
+    # meat4 = Meat()
+    # vegetable4 = Vegetable()
+    # meat5 = Meat()
+    # vegetable5 = Vegetable()
+    # meat6 = Meat()
+    # vegetable6 = Vegetable()
+    #
+    # mouth = Mouth();
+    # mouth.start()
+    #
+    # mouth.add(meat1)
+    # mouth.add(vegetable1)
+    #
+    # sleep(1)
+    #
+    # mouth.add(meat2)
+    # mouth.add(vegetable2)
+    #
+    # sleep(1)
+    #
+    # mouth.add(meat3)
+    # mouth.add(vegetable3)
+    #
+    # sleep(1)
+    #
+    # mouth.add(meat4)
+    # mouth.add(vegetable4)
+    #
+    # sleep(1)
 
-    sleep(1)
 
-    mouth.add(meat2)
-    mouth.add(vegetable2)
-
-    sleep(1)
-
-    mouth.add(meat3)
-    mouth.add(vegetable3)
-
-    sleep(1)
-
-    mouth.add(meat4)
-    mouth.add(vegetable4)
-
-    sleep(1)
